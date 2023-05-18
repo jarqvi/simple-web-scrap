dc489bd961368d5b58a59fae648eaee7a6c980c6
MohammadHasan Akbari <116190942+jarqvi@users.noreply.github.com>
2023-05-18 09:56:37 +0000

    Receive the address and name and date of the stock price

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 
-async function run() {
+// const output = [
+//     {
+//         title: '',
+//         data: '',
+//         url: '',
+//         percent: '',
+//         stratrgy: '',
+//         price: '',
+//         buyer: '',
+//         seller: '',
+//         volumeBuy: '',
+//         volumeSell: ''
+
+//     }
+// ];
+
+(async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://toplearn.com/');
-    browser.close();
-}
-run();
+    await page.goto('https://rahavard365.com/stock');
+    const html = await page.content();
+    const $ = cheerio.load(html);
+    const result = $('table > tbody > tr').map((i, e) => {
+        const titleElement = $(e).find('.symbol');
+        const title = $(titleElement).text();
+        const url = 'https://rahavard365.com' + $(titleElement).attr('href');
+        const dateElement = $(e).find('[data-type="date-time-data"]');
+        const date = new Date($(dateElement).attr('data-order'));
+        const percentElement = $(e).find('[data-type="real-close-price-change-percent-data"]');
+        const percent = $(percentElement).attr('data-order');
+        return {title, url, date, percent};
+    }).get();
+    console.log(result);
+}())
